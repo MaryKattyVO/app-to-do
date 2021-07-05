843048ef55b82c6bd44b659c75eff5fa070ec736
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-04 23:00:19 -0500

    localStorage para mantener la información de la lista

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Injectable } from '@angular/core';
 import { Lista } from '../pages/models/lista.model';
 
@@ -9,15 +10,26 @@ export class DeseosService {
 
 
   constructor() {
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Héroes a desaparecer');
-
-    this.listas.push(lista1, lista2);
+    this.cargarStorage();
   }
 
   crearLista( titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+
+    if( localStorage.getItem('data') ){
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
   }
 
 
